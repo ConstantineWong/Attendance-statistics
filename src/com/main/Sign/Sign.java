319631c6bcedc6bca55cf7f595fdc64bcc949629
WangJun <2858600511@qq.com>
2020-10-31 10:32:32 +0800

    2020-10-26~2020-10-30日打卡

--- src/com/main/Sign/Sign.java
+++ src/com/main/Sign/Sign.java
@@ -27,8 +27,8 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  */
 public class Sign {
     public static void main(String[] args) throws IOException {
-        String dingding = "上海海洋大学智慧海洋_考勤报表_20201019-20201023";
-        String leave = "leave20201019-20201023";
+        String dingding = "上海海洋大学智慧海洋_考勤报表_20201026-20201030";
+        String leave = "leave20201026-20201030";
         String white_list = "白名单";
         String gradeOne_str="研一同学";
         int day = 5;    //统计的天数
